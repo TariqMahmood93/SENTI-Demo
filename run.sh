2265f861cc1d1ec6f9fac8192045c1dd62f8236e
Tariq Mahmood <mahmood.tariq@dimes.unical.it>
2026-03-14 02:30:16 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+VENV=".venv"
+if [ ! -d "$VENV" ]; then
+    python3 -m venv "$VENV"
+fi
+source "$VENV/bin/activate"
+pip install -q -r requirements.txt
+streamlit run TIDI.py --server.port 8501
